0fa4f12d8b6158d24c39fc3df53b60aed2f2026a
jackhein <moustiquejunior@gmail.com>
2023-08-06 21:05:25 +0200

    Add taking in account mod

--- migration.sh
+++ migration.sh
@@ -5,6 +5,7 @@
 ############################################################
 # Source progress bar
 MIGRATION_CHARACTERS_FILE=('1_CREATE_CLEANUP_TABLES' '2_CREATE_MISSING_TABLES' '3_ALTER_TABLES' '4_CLEANUP_AND_CONVERT_SPELLS' '5_FINAL_CLEANUP')
+MIGRATION_MOD=()
 MIGRATION_AUTH_FILE=('6.AUTH_RENAME_COLUMNS' '7.AUTH_CLEAN_AND_CONVERTER')
 
 
@@ -21,12 +22,23 @@ Help()
    # Display Help
    echo "Restore & migrate TrinityCore DB to AzerothCore DB"
    echo
-   echo "Syntax: scriptTemplate [-a|-r|-m]"
+   echo "Syntax: scriptTemplate ([-d][MOD_NAME])...  [-a|-r|-m]"
    echo "options:"
    echo "a     Restore and migrate DB"
    echo "r     Restore original TrinityCore DB"
    echo "m     Migrate TrinityCore DB to AzerothCore DB"
-   echo "c     Migrate only Characters table from TrinityCore DB to AzerothCore DB"
+   echo "d     Taking account of (known) mod used in TrinityCore DB to AzerothCore DB migration"
+   echo "h     Show this message"
+   echo
+   echo "Exemple: ./migrate -d mod_transmog -d mod_other -a"
+   #echo "c     Migrate only Characters table from TrinityCore DB to AzerothCore DB"
+}
+
+HelpModule()
+{
+  # Dipslay known modules for migration
+  echo "Known modules:"
+  echo "       MOD_TRANSMOG"
 }
 
 Restore()
@@ -100,30 +112,55 @@ MigrateCharacters()
   done
 }
 
+MigrateCharactersMod()
+{
+  for db_index in "${!CHARACTERS_DBS_ARRAY[@]}"; do
+    RepeatChar '-'
+    echo "Migrate ${CHARACTERS_DBS_ARRAY[$db_index]}"
+    RepeatChar '-'
+    for file_name in "${MIGRATION_MOD[@]^^}"
+    do
+      echo "Execute: $file_name"
+      /usr/bin/mysql --database="${CHARACTERS_DBS_ARRAY[$db_index]}"\
+      --user="${DB_USER}"\
+      --password="${DB_PASS}"\
+      --host="${DB_HOSTNAME}"\
+      --port="${DB_PORT}"\
+      < "mod/${file_name}.sql"
+    done
+  done
+}
+
 ############################################################
 # Main program                                             #
 ############################################################
 # Get the options
-while getopts ":hrma" option; do
+while getopts ":hrmad:" option; do
    case $option in
+      d) # Enter mod name in capital letter (like following MOD_TRANSMOG)
+	 MIGRATION_MOD+=("$OPTARG");;
       h) # display Help
          Help
+	 HelpModule
          exit;;
       r) # Enter a name
         Restore
         exit;;
       m) # Enter a name
         MigrateCharacters
+	MigrateCharactersMod
         MigrateAuth
         exit;;
       a) # Enter a name
         Restore
         MigrateCharacters
+	MigrateCharactersMod
         MigrateAuth
         exit;;
      \?) # Invalid option
-         Help
-         exit;;
+	echo "Invalid option."
+        Help
+        exit;;
    esac
 done
 # migrate dbs
