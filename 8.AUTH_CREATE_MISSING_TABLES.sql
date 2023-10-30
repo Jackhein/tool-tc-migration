da86d09782cc9029eba0952de005476d2d2bb0dd
jackhein <moustiquejunior@gmail.com>
2023-10-30 23:29:27 +0100

    correct realmid default value for AUTH.motd

--- 8.AUTH_CREATE_MISSING_TABLES.sql
+++ 8.AUTH_CREATE_MISSING_TABLES.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `motd` (
-  `realmid` INT UNSIGNED NOT NULL DEFAULT NULL,
+  `realmid` INT UNSIGNED NOT NULL DEFAULT 0,
   `text` longtext DEFAULT NULL,
   PRIMARY KEY (`realmid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
