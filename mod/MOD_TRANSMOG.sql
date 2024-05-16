dd0b0c9396c59eb8f345a8935c3761d33f3493ab
jackhein <moustiquejunior@gmail.com>
2024-05-16 16:12:51 +0200

    MOD_TRANSMOG.sql: correct remove of column

--- mod/MOD_TRANSMOG.sql
+++ mod/MOD_TRANSMOG.sql
@@ -7,6 +7,13 @@ CREATE TABLE IF NOT EXISTS `custom_transmogrification` (
   KEY `Owner` (`Owner`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='6_2';
 
+-- MOD_TRANSMOGRIFICATION - Add table custom_unlocked_appearances
+CREATE TABLE IF NOT EXISTS `custom_unlocked_appearances` (
+    `account_id`       int(10) unsigned      NOT NULL,
+    `item_template_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+    PRIMARY KEY (`account_id`, `item_template_id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
 -- MOD_TRANSMOGRIFICATION - Copy existing transmogrification in new table custom_transmogrification
 INSERT IGNORE INTO `custom_transmogrification` (`GUID`, `Owner`, `FakeEntry`)
 SELECT `guid`, `owner_guid`, `transmog`
@@ -15,10 +22,3 @@ WHERE `transmog` != 0 and `transmog` != `itemEntry`;
 
 -- MOD_TRANSMOGRIFICATION - Remove unused column
 ALTER TABLE `item_instance` DROP COLUMN `transmog`;
-
--- MOD_TRANSMOGRIFICATION - Add table custom_unlocked_appearances
-CREATE TABLE IF NOT EXISTS `custom_unlocked_appearances` (
-    `account_id`       int(10) unsigned      NOT NULL,
-    `item_template_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-    PRIMARY KEY (`account_id`, `item_template_id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
