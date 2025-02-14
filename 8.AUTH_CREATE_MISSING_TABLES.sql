47adfe39832097051990062174a953de6c0733f5
jackhein <moustiquejunior@gmail.com>
2025-02-14 17:02:15 +0100

    2024_12_15 update: add of motd_localized

--- 8.AUTH_CREATE_MISSING_TABLES.sql
+++ 8.AUTH_CREATE_MISSING_TABLES.sql
@@ -3,3 +3,10 @@ CREATE TABLE `motd` (
   `text` longtext DEFAULT NULL,
   PRIMARY KEY (`realmid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS `motd_localized` (
+  `realmid` INT,
+  `locale` VARCHAR(4) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+  `text` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
+  PRIMARY KEY (`realmid`, `locale`)
+) CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci ENGINE = InnoDB;
+
